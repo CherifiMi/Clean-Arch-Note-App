39ae86fec654d35f75bdc6b8c452088be70bca41
CherifiMi <cherifi154949@gmail.com>
2022-06-21 23:10:03 +0100

    repository done

---
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/data/local/NoteDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.cleanarchnoteapp.feature_note.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.cleanarchnoteapp.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabas: RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+
+}
