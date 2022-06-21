37f64815c18f9f60ac65103d4dcd296886243cc2
CherifiMi <cherifi154949@gmail.com>
2022-06-21 22:49:17 +0100

    added packages

--- app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/MainActivity.kt
@@ -1,13 +1,20 @@
-package com.example.cleanarchnoteapp
+package com.example.cleanarchnoteapp.feature_note.presentasion
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.cleanarchnoteapp.ui.theme.CleanArchNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,13 +33,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Text(
+            text = "Hello $name!",
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CleanArchNoteAppTheme {
-        Greeting("Android")
+        Greeting("Mito")
     }
 }
