5077187d2e2b1a364664a3acc03afa2f33a2e1c7
CherifiMi <cherifi154949@gmail.com>
2022-06-22 04:33:34 +0100

    making the di module

---
+++ app/src/main/java/com/example/cleanarchnoteapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.example.cleanarchnoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.cleanarchnoteapp.feature_note.data.local.NoteDatabas
+import com.example.cleanarchnoteapp.feature_note.data.repository.NoteRepositoryImp
+import com.example.cleanarchnoteapp.feature_note.domain.repository.NoteRepository
+import com.example.cleanarchnoteapp.feature_note.domain.use_case.DeleteNote
+import com.example.cleanarchnoteapp.feature_note.domain.use_case.GetNotes
+import com.example.cleanarchnoteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabas{
+        return Room.databaseBuilder(
+            app,
+            NoteDatabas::class.java,
+            NoteDatabas.DATABSE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabas): NoteRepository{
+        return NoteRepositoryImp(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+
+}
