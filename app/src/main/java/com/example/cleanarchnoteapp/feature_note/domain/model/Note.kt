dd8908ba72572e62f628c4b4fdd7ade74a3c4404
CherifiMi <cherifi154949@gmail.com>
2022-06-26 03:26:02 +0100

    some fixes

--- app/src/main/java/com/example/cleanarchnoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/domain/model/Note.kt
@@ -10,7 +10,7 @@ data class Note(
     val title: String,
     val content: String,
     val timestamp: Long,
-    val color: String,
+    val color: Int,
 ){
     companion object{
         val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
