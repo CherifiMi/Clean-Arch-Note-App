2df81603e31e9e119de9537dc8c5f8b88f539700
CherifiMi <cherifi154949@gmail.com>
2022-06-26 17:38:30 +0100

    added get note by id use case

--- app/src/main/java/com/example/cleanarchnoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -2,6 +2,7 @@ package com.example.cleanarchnoteapp.feature_note.domain.use_case
 
 data class NoteUseCases(
     val getNotes: GetNotes,
+    val getNote: GetNote,
     val deleteNote: DeleteNote,
     val insertNote: InsertNote
 )
