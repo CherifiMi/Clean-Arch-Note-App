96282338d6d7126fa6b1e223ee01366a39ade8fe
CherifiMi <cherifi154949@gmail.com>
2022-06-26 03:26:23 +0100

    created note item

---
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NoteItem.kt
@@ -0,0 +1,90 @@
+package com.example.cleanarchnoteapp.feature_note.presentasion.note_list.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.cleanarchnoteapp.feature_note.domain.model.Note
+
+@Composable
+fun NoteItem(
+    note: Note,
+    modifier: Modifier,
+    cutCorner: Dp = 30.dp,
+    corner: Dp = 10.dp,
+    onDeleteClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Canvas(modifier = Modifier.matchParentSize()) {
+            val clipPath = Path().apply {
+                lineTo(size.width - cutCorner.toPx(), 0f)
+                lineTo(size.width, cutCorner.toPx())
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+                close()
+            }
+            clipPath(clipPath) {
+                drawRoundRect(
+                    color = Color(note.color),
+                    size = size,
+                    cornerRadius = CornerRadius(corner.toPx())
+                )
+                drawRoundRect(
+                    color = Color(0x75202020),
+                    topLeft = Offset(size.width - cutCorner.toPx(), -100f),
+                    size = Size(cutCorner.toPx() + 100f, cutCorner.toPx() + 100f),
+                    cornerRadius = CornerRadius(corner.toPx())
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 32.dp),
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.h6,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 10,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        IconButton(
+            onClick = {onDeleteClick},
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ){
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Note")
+        }
+    }
+}
