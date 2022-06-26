af25e102cf0154f7b9fd4b7c3bd980496224e3c7
CherifiMi <cherifi154949@gmail.com>
2022-06-26 04:24:10 +0100

    note item done

--- app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NoteItem.kt
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NoteItem.kt
@@ -1,6 +1,5 @@
 package com.example.cleanarchnoteapp.feature_note.presentasion.note_list.components
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
