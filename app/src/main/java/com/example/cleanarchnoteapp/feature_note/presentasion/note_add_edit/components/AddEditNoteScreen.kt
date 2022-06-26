61311ea602196f5f27ac0ee83ef6112270b6580d
CherifiMi <cherifi154949@gmail.com>
2022-06-26 19:59:48 +0100

    init add edit note screen

---
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_add_edit/components/AddEditNoteScreen.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchnoteapp.feature_note.presentasion.note_add_edit.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddEditNoteScreen() {
+    
+}
