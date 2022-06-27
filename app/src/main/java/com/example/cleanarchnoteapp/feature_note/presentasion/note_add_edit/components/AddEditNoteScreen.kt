06720106baeb234233f0dc2be49e5292d95dc3d8
CherifiMi <cherifi154949@gmail.com>
2022-06-27 01:46:37 +0100

    added the top color changing things

--- app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_add_edit/components/AddEditNoteScreen.kt
+++ app/src/main/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_add_edit/components/AddEditNoteScreen.kt
@@ -1,8 +1,104 @@
 package com.example.cleanarchnoteapp.feature_note.presentasion.note_add_edit.components
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.cleanarchnoteapp.feature_note.domain.model.Note
+import com.example.cleanarchnoteapp.feature_note.presentasion.note_add_edit.AddEditNoteEvents
+import com.example.cleanarchnoteapp.feature_note.presentasion.note_add_edit.AddEditViewModel
+import kotlinx.coroutines.launch
 
 @Composable
-fun AddEditNoteScreen() {
-    
+fun AddEditNoteScreen(
+    navController: NavController,
+    noteColor: Int,
+    viewModel: AddEditViewModel = hiltViewModel()
+) {
+    val titleState = viewModel.noteTitle.value
+    val decState = viewModel.noteDec.value
+
+    val scaffoldState = rememberScaffoldState()
+
+    val noteColorAnimatable = remember {
+        Animatable(
+            Color(if (noteColor!= -1) noteColor else viewModel.noteColor.value)
+        )
+    }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    viewModel.onEvent(AddEditNoteEvents.SaveNote)
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.Save, contentDescription = "Save note")
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(noteColorAnimatable.value)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Note.noteColors.forEach {color->
+                    val colorInt = color.toArgb()
+
+                    Box (
+                        modifier = Modifier
+                            .size(50.dp)
+                            .shadow(15.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .border(
+                                width = 3.dp,
+                                color = if(viewModel.noteColor.value == colorInt) Color.Black else Color.Transparent,
+                                shape = CircleShape
+                            )
+                            .clickable {
+                                scope.launch {
+                                    noteColorAnimatable.animateTo(
+                                        targetValue = color,
+                                        animationSpec = tween(
+                                            durationMillis = 500,
+                                        )
+                                    )
+                                }
+                                viewModel.onEvent(AddEditNoteEvents.ChangeColor(colorInt))
+                            }
+                            )
+                }
+            }
+        }
+    }
 }
+
+
