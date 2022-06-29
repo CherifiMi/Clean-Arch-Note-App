c1290c28a23e514c0421876d56167b306b375715
CherifiMi <cherifi154949@gmail.com>
2022-06-29 23:04:44 +0100

    clickToggleOrderThing_isVisible first test done

--- app/src/androidTest/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NotesScreenKtTest.kt
+++ app/src/androidTest/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NotesScreenKtTest.kt
@@ -1,10 +1,17 @@
 package com.example.cleanarchnoteapp.feature_note.presentasion.note_list.components
 
+import android.content.Context
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.test.core.app.ApplicationProvider
+import com.example.cleanarchnoteapp.core.util.TestTags
 import com.example.cleanarchnoteapp.di.AppModule
 import com.example.cleanarchnoteapp.feature_note.presentasion.MainActivity
 import com.example.cleanarchnoteapp.feature_note.presentasion.util.Screen
@@ -15,6 +22,7 @@ import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.manipulation.Ordering
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
@@ -51,7 +59,10 @@ class NotesScreenKtTest {
 
     @Test
     fun clickToggleOrderThing_isVisible(){
-
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertIsDisplayed()
     }
 
 }
