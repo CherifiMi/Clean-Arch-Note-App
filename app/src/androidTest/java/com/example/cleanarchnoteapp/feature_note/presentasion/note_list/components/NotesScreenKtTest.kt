337fd3acb57a811e43a4b6278307f6ca2275594d
CherifiMi <cherifi154949@gmail.com>
2022-06-29 22:39:17 +0100

    told test class to not use the real di module

--- app/src/androidTest/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NotesScreenKtTest.kt
+++ app/src/androidTest/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NotesScreenKtTest.kt
@@ -1,8 +1,11 @@
 package com.example.cleanarchnoteapp.feature_note.presentasion.note_list.components
 
+import com.example.cleanarchnoteapp.di.AppModule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 
 @HiltAndroidTest
+@UninstallModules(AppModule::class)
 class NotesScreenKtTest{
 
 }
