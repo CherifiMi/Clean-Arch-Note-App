74130f22203b8c6a1ccf3a98e5bebf4835451367
CherifiMi <cherifi154949@gmail.com>
2022-06-29 22:19:53 +0100

    NotesScreenKtTest.kt init

---
+++ app/src/androidTest/java/com/example/cleanarchnoteapp/feature_note/presentasion/note_list/components/NotesScreenKtTest.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchnoteapp.feature_note.presentasion.note_list.components
+
+import dagger.hilt.android.testing.HiltAndroidTest
+
+@HiltAndroidTest
+class NotesScreenKtTest{
+
+}
